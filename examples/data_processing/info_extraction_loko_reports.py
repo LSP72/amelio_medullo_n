094960aa261e7ced74a4addf6d83e3274f3f0217
Math <math.math@caramail.fr>
2026-03-20 14:09:40 +0100

    info_extraction_loko_reports: script to export the info from loko report

---
+++ examples/data_processing/info_extraction_loko_reports.py
@@ -0,0 +1,21 @@
+from openpyxl import load_workbook
+import os
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+from amelio_medullo import ProcessDataLokomat
+
+def main():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+
+    folder_path = filedialog.askdirectory()
+
+    print("Selected folder:", folder_path)
+
+    name_output_file = input("\nEnter the name of the output file: ")
+
+    ProcessDataLokomat.read_data_info(folder_path, name_output_file)
+
+if __name__ == "__main__":
+    main()
