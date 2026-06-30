271131213caefe1cfecb42fef675049eccb18304
Math <math.math@caramail.fr>
2026-06-30 19:58:30 -0400

    test

---
+++ examples/data_processing/testings_processing/bilan_muscul_combined.py
@@ -0,0 +1,83 @@
+from amelio_medullo import MuscleScore
+import pandas as pd
+import os
+
+import tkinter as tk
+from tkinter import filedialog
+
+
+def combine_muscle_scores(df, mapping_dict, cols_to_add, side=None):
+    if side:
+        mapping_dict = MuscleScore.transform_dict_to_side(mapping_dict, side)
+
+    movement_scores = MuscleScore.convert_muscles_to_movements(df, mapping_dict)
+
+    for col in cols_to_add:
+        if col not in movement_scores.columns.to_list():
+            movement_scores = movement_scores.merge(df[["IPP", col]], on="IPP", how="right")
+
+    return movement_scores
+
+
+def add_muscular_scores(df, mapping_dict, cols_to_add, selected_leg: bool = True):
+    if selected_leg == False:
+        right_scores = combine_muscle_scores(df, mapping_dict, cols_to_add, "right")
+        left_scores = combine_muscle_scores(df, mapping_dict, cols_to_add, "left")
+
+        combined_scores_df = right_scores.combine_first(left_scores)
+        print(combined_scores_df.head())
+
+    elif selected_leg == True:
+        combined_scores_df = combine_muscle_scores(df, mapping_dict, cols_to_add)
+        print(combined_scores_df.head())
+
+    return combined_scores_df
+
+
+def main(dict_mvt_BM, cols_to_add, file_path=None, selected_leg=False, save=False):
+    if file_path is None:
+        root = tk.Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(
+            title="Select the data file", filetypes=[("Excel files", "*.xlsx"), ("All files", "*.*")]
+        )
+
+    data = pd.read_excel(file_path)
+
+    combined_scores_df = add_muscular_scores(data, dict_mvt_BM, cols_to_add, selected_leg)
+
+    if save == True:
+        directory_path = os.path.dirname(file_path)
+        output_file = directory_path + "/combined_movement_scores.xlsx"
+        combined_scores_df.to_excel(output_file, index=False)
+    print(f"Combined movement scores have been saved to {output_file}.")
+
+    return combined_scores_df
+
+
+if __name__ == "__main__":
+
+    dict_mvt_BM = {
+        "H_Flex_ass": ["Sartorius", "Iliopsoas"],
+        "H_Ext_PP": ["Gmax"],
+        "H_abd": ["GM"],
+        "H_add": ["Adductor"],
+        "H_rot_int": ["Gm"],
+        "K_Flex": ["SmTD", "Smbr", "Bic_Fem"],
+        "K_Ext": ["RF", "QF", "Gracilis"],
+        "A_Dorsiflex_GT": ["TA"],
+        "A_Plantarflex": ["Gastroc", "Sol"],
+        "A_Ever": ["Fibu_long"],
+        "A_Inver": ["TP"],
+    }
+    cols_to_add = [
+        "H_Ext_GF_D_pre",
+        "H_Ext_GF_G_pre",
+        "A_Dorsiflex_GF_D_pre",
+        "A_Dorsiflex_GF_G_pre",
+    ]
+
+    selected_leg = False
+    file_path = "/Users/mathildetardif/Documents/Documents/PhD/Nantes/m_a_testings_pre_post_data.xlsx"
+
+    main(dict_mvt_BM, cols_to_add, file_path, selected_leg, save=True)
